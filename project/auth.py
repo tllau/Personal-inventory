5fe6968a6cc2092870aa99d391d702cd8937b028
tllau <tllau0310@gmail.com>
2021-07-26 22:49:16 +0800

    feat: add loginmanager for basic login, relocate static folder

--- project/auth.py
+++ project/auth.py
@@ -1,15 +1,29 @@
+from flask_login import login_user
 from flask import Blueprint, render_template, redirect, request, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .model import User
+import sys
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["Get", "POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email=email).first()
+        remember = False
+        if not user or not check_password_hash(user.password, password):
+            flash("Wrong email or password")
+            return redirect(url_for("auth.login"))
+        else:
+            login_user(user, remember=remember)
+            return redirect(url_for("main.profile"))
+    else:           
+        return render_template("login.html")
 
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
         email = request.form["email"]
@@ -32,4 +46,5 @@ def register():
 
 @auth.route('/logout')
 def logout():
+        
     return render_template('logout.html')
